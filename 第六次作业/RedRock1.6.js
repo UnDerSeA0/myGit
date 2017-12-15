6cfb536ab078e6f6f43f9b6e4b3abe6a5281443b
UnDerSeA0 <1070742799@qq.com>
2017-12-16 01:06:12 +0800

    边上会多一点，我明早再试试

--- 第六次作业/RedRock1.6.js
+++ 第六次作业/RedRock1.6.js
@@ -7,6 +7,7 @@ window.onload = function () {
     var timer;
     var moveStep = 10;
     var Jweather;
+    var canDrag = 0;
     bodyDiv.appendChild(oDiv);
     oDiv.style.width = 100 + "px";
     oDiv.style.height = 100 + "px";
@@ -68,6 +69,40 @@ window.onload = function () {
             +Math.floor(Math.random()*255)+')';
         oDiv.style.backgroundColor = rgb;
 
+    }
+
+    //拖动
+    var mouseBiasX = 0;
+    oDiv.addEventListener("mousedown",function (e) {
+        var e =e ;
+        mouseBiasX = e.pageX -  oDiv.offsetLeft;
+        mouseBiasY = e.pageY - oDiv.offsetTop;
+       canDrag = 1;
+    });
+    document.onmousemove = function (e) {
+        var e =e;
+        var mouseX = e.pageX;
+        var mouseY = e.pageY;
+        var moveX = 0;
+        var moveY = 0;
+        if(canDrag)
+        {
+            moveX = mouseX -mouseBiasX;
+            moveY = mouseY - mouseBiasX;
+            if(moveX<0)
+                moveX = 0;
+            if(moveX>document.body.clientWidth)
+                moveX =document.body.clientWidth;
+            if(moveY<0)
+                moveY = 0;
+            if(moveY>document.body.clientHeight)
+                moveY = document.body.clientHeight;
+            oDiv.style.left = moveX + 'px';
+            oDiv.style.top  = moveY + 'px';
+        }
+    }
+    document.onmouseup = function () {
+        canDrag = 0;
     }
 
 };
